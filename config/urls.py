9cf00ed8262b97beee3a639b46aaf69bb0ed3754
Mazlum Ağar <mazlum.agar@gmail.com>
2020-06-04 14:46:10 +0300

    project structure refactored

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from posts import views
+from kodilan.posts import views
 
 router = routers.DefaultRouter()
 
---
+++ kodilan/baseapp/pagination.py
@@ -0,0 +1,21 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "links": {
+                    "next": self.get_next_link(),
+                    "previous": self.get_previous_link(),
+                },
+                "count": self.page.paginator.count,
+                "results": data,
+                "page_number": self.page.number,
+            }
+        )
